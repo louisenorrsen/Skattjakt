ed603414e10efa0128773b9706528c34816ef3c7
Louise Norrsén <louisenorrsen@outlook.com>
2024-05-24 17:42:44 +0200

    add Station and LocationCoordinate

--- Data/Station.cs
+++ Data/Station.cs
@@ -1,16 +1,21 @@
 ﻿namespace Skattjakt.Data
 {
-    public struct LocationCoordinates
+    public struct LocationCoordinate
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public LocationCoordinate(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
     }
 
     public class Station
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public LocationCoordinates Location { get; set; }
+        public LocationCoordinate Location { get; set; }
         public string Assignment { get; set; }
     }
 }
