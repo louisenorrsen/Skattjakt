4650e174776ab8a0db751514ecc1922312211bd2
Louise Norrsén <louisenorrsen@outlook.com>
2024-05-24 13:56:49 +0200

    add Station class

---
+++ Data/Station.cs
@@ -0,0 +1,16 @@
+﻿namespace Skattjakt.Data
+{
+    public struct LocationCoordinates
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public LocationCoordinates Location { get; set; }
+        public string Assignment { get; set; }
+    }
+}
