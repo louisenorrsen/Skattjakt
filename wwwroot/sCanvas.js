d2c30a90d5bef21fd642e27176c9932da5d8a4a4
Alex Wigforss <alex.wigforss@gmail.com>
2024-06-04 10:16:58 +0200

    calibrated compas

--- wwwroot/sCanvas.js
+++ wwwroot/sCanvas.js
@@ -4,15 +4,20 @@
             var canvas = document.getElementById("myCanvas");
             var ctx = canvas.getContext("2d");
             var width = canvas.width;
+            canvas.height = canvas.width;
             var height = canvas.height;
 
             // Set the fill color for the background
-            ctx.fillStyle = "#ADD8E6"; // Light blue color; change this to any color you like
+            ctx.fillStyle = "#FFFFFF"; // Light blue color; change this to any color you like
             // Fill the entire canvas with the selected color
             ctx.fillRect(0, 0, width, height);
 
             ctx.moveTo(width / 2, height / 2);
             ctx.lineTo(width / 2 + w, height / 2 + h);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(width / 2, height / 2, height/2, 0, 2 * Math.PI);
             ctx.stroke();
 
             ctx.beginPath();
